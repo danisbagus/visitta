70ed454930717d5684ae9f1ee8f1c3f4fd28fb89
Danis Bagus Setiawan <danis.bagus@majoo.id>
2022-12-01 11:26:55 +0700

    add prebuild script

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ app.use((req, res, next) => {
 // routes
 routes(app);
 
+console.log(`env: ${process.env.NODE_ENV}`);
+
 app.listen(config.port, () => {
   console.log(`Running app on port ${config.port}`);
 });
